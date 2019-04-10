db2601f26a4598554ff5b75ba1af640e8bd1e235
Alex Abbott <41922804+RAAbbott@users.noreply.github.com>
2019-04-09 22:47:53 -0600

    Added documentation through comments for my code

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-let formattedCSV = '';
+let csvResult = '';
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Handles the post requests coming into the server
+// Handles the post requests coming into the server and returns the result
 app.post('/formatCSV', function(req, res) {
-  formattedCSV = formatCSV(req.body.csv);
-  res.setHeader('Content-Type', 'text/plain');
-  res.send(formattedCSV);
+  csvResult = formatCSV(req.body.csv);
+  res.setHeader('Content-Type', 'text/plain'); // text/plain is needed for newlines to work in response
+  res.send(csvResult);
 });
 
 
@@ -18,38 +18,24 @@ app.listen(3000, function() {
 });
 
 
-
 // Function to format the input CSV string
-
 function formatCSV(str) {
-  if (!str) {
+  if (!str) {     // Verifies that str exists
     return "Input was null";
   }
-  // New Array to hold the completely formatted CSV input data
-  let formattedCSV = [];
-  // Determines whether newline character is \n or \r\n
-  let newLineChar = (str.indexOf('\n') == -1) ? '\r\n' : '\n';
-  console.log(newLineChar);
-  // Splits the entire CSV string into rows by using the newline character
-  str.split(newLineChar).forEach(row => {
-    // New Array to hold each formatted row
-    let formattedRow = [];
-    // Splits on each quotation mark to get an array of the items within the row
-    row.split('"').forEach(item => {
-      // If statement basically eliminates items that are [] or [,]
-      if (item.length > 2) {
-        // This ternary is used to catch the 'Age' property, it doesn't have quotes
-        // so it appears in the row as ",21,". This removes the commas if present
-        let formattedItem = (item[0] == ',') ? item.slice(1,item.length - 1) : item;
-        // Adds each item between brackets to its row
-        formattedRow.push("[" + formattedItem + "]");
+  let formattedCSV = [];      // New array to hold the completely formatted CSV input data
+  let newLineChar = (str.indexOf('\n') == -1) ? '\r\n' : '\n';      // Determines whether newline character is \n or \r\n
+  str.split(newLineChar).forEach(row => {     // Splits the entire CSV string into rows by using the newline character
+    let formattedRow = [];                    // New array to hold each formatted row
+    row.split('"').forEach(item => {          // Splits on each quotation mark to get an array of the items within the row
+      if (item.length > 2) {                  // If statement basically eliminates items that are [] or [,]
+        let formattedItem = (item[0] == ',') ? item.slice(1,item.length - 1) : item; // Age doesn't have quotes so it appears in the row as ",21,". This removes the commas if present
+        formattedRow.push("[" + formattedItem + "]");       // Adds each item between brackets to its row
       }
     });
-    // Adds each row to the complete array
-    formattedCSV.push(formattedRow.join(' '));
+    formattedCSV.push(formattedRow.join(' '));      // Adds each row to the complete array
   });
-  console.log(formattedCSV.join(newLineChar).toString());
-  // Returns complete array as string, with '\n' between each row
-  return formattedCSV.join(newLineChar).toString();
+  console.log(formattedCSV.join(newLineChar).toString());       // Logs complete array to the console
+  return formattedCSV.join(newLineChar).toString();       // Returns complete array as string, with '\n' between each row
 }
 
